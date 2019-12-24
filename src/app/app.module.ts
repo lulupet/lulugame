fda00d8c685425664df03c79843599ba822a5ab0
Lucas Petit <lucas.petit@riminder.net>
2019-12-24 17:02:39 +0100

    feat:init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HandComponent } from './hand/hand.component';
+import { StacksComponent } from './stacks/stacks.component';
+import { GameService } from './game.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HandComponent,
+    StacksComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
